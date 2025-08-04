c698ca2bc112696e9263823507d916f8d82610f1
Arslan <devarslan457@gmail.com>
2025-08-04 13:32:55 +0530

    add some fields in productController

--- controllers/productController.js
+++ controllers/productController.js
@@ -1,15 +1,26 @@
 
-const addProduct = async (req, res) =>{
+const  addProduct = async (req, res) =>{
 
     try {
 
-        const {name, description, price, image, category, subCategory, sizes, bestSell,}  = req.body ; 
-        console.log("wirte")
+        const {name, description, price,  category, subCategory, sizes,  bestSeller,}  = req.body ; 
+
+        const image1 = req.files.image1 && req.files.image1[0] ;
+        const image2 =  req.files.image2 && req.files.image2[0] ;
+        const image3 =  req.files.image3 && req.files.image3[0] ;
+        const image4 =  req.files.image4 && req.files.image4[0] ;
+        
+
+        console.log(name,description,price, category, subCategory, sizes, bestSeller,) ;
+        
+        console.log(image1, image2, image3, image4) 
+        res.json({success:true})
+        
         
     } catch (error) {
-        console.log(error.message) ;
+        console.log(error.message,'add error') ;
 
-        res.json({success:true})
+        res.json({success:false, msg:error.message })
     }
 
 } ;
--- models/productModel.js
+++ models/productModel.js
@@ -5,11 +5,11 @@ const productSchema =  new mongoose.Schema({
     name:{type:String, required: true},
       description:{type:String, required: true},
         price:{type:Number, required: true},
-          image:{type:Array, required: true},
+          // image:{type:Array, required: true},
             category:{type:String, required: true},
               subCategory:{type:String, required: true},
                 sizes:{type:String, required: true},
-                  name:{type:Boolean },
+                  bestSeller:{type:Boolean },
                     date:{type:Number, required: true},
 }) ;
 
